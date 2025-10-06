968fecd71439a71e41c09e98f028b489f64fc31c
B00981579 <kavanagh-n7@ulster.ac.uk>
2025-10-06 15:32:36 +0100

    updated code with variables and properties

--- Week3-Library/Book.cs
+++ Week3-Library/Book.cs
@@ -2,23 +2,40 @@
 {
     class Book
     {
-        string Title;
-        string Author;
-        string ISBN;
+        private string title; //variable
+        private string author; //variable
+        private string isbn; //variable
+
+        //Title property to allow access to the private Title variable
+        public string Title
+        {
+            get { return title; } //get method
+            set { title = value; } //set method
+        }
+        public string Author
+            {
+            get { return author; } //get method
+            set { author = value; } //set method
+        }
+        public string Isbn
+        {
+            get { return isbn; } //get method
+            set { isbn = value; } //set method
+        }
 
         //Example of a constructor that allows us to "construct" a new book object
         public Book(string bookTitle, string bookAuthor, string bookISBN)
         {
-            Title = bookTitle;
-            Author = bookAuthor;
-            ISBN = bookISBN;
+            this.title = bookTitle;
+            this.author = bookAuthor;
+            this.isbn = bookISBN;
         }
 
         public void DisplayInfo()
         {
-            Console.WriteLine($"Title: {Title}");
-            Console.WriteLine($"Author: {Author}");
-            Console.WriteLine($"ISBN: {ISBN}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Author: {author}");
+            Console.WriteLine($"ISBN: {isbn}");
             Console.WriteLine();
         }
     }
